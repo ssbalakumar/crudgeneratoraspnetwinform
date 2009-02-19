0fe03fe78fa69873433c1e54122f3530ce7385dc
ehassen <ehassen@f1900920-ea99-11dd-8fb1-9960f2a117f8>
2009-02-19 02:48:06 +0000

    Finished work on the c# Business layer development phase.  Now to begin work on the data layer and the session state saving algorithm.

--- trunk/CrudGenerator/Util/Utility.cs
+++ trunk/CrudGenerator/Util/Utility.cs
@@ -7,11 +7,24 @@ namespace CrudGenerator.Util
     class Utility
     {
         private static string _connStr="";
+        public static SettingsData UserSettings;
 
+        
         public static string ConnectionString {
-
-            get{return _connStr;}
-            set { _connStr = value; }
+            get{
+                if (UserSettings != null)
+                    return UserSettings.ConnectionString;
+                else return _connStr;
+            }
+            set {
+                if (UserSettings != null)
+                    UserSettings.ConnectionString = value;
+                _connStr = value; 
+            }
         }
+
+        
+       
+        
     }
 }
